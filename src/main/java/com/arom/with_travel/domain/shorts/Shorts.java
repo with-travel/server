5c2e3999e6b8c6b47421c57d1e25a892f9aebc42
LeeDongHoon <likelion.leedh@gmail.com>
2025-01-20 15:57:52 +0900

    [feat] Shorts 엔티티 초안 설계

---
+++ src/main/java/com/arom/with_travel/domain/shorts/Shorts.java
@@ -0,0 +1,33 @@
+package com.arom.with_travel.domain.shorts;
+
+import com.arom.with_travel.domain.member.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE shorts SET deleted = true where id = ?")
+@SQLRestriction("deleted is FALSE")
+public class Shorts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull private String url;
+    @NotNull private String title;
+    @Lob private String caption;
+    private Long views = 0L;
+    private boolean deleted = Boolean.FALSE;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+}
