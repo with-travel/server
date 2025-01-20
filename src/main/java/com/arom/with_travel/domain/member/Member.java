729f16d476f84ef3fe825222ef598d1cfa1645e7
LeeDongHoon <likelion.leedh@gmail.com>
2025-01-20 15:57:27 +0900

    [feat] Member 엔티티 초안 설계

--- src/main/java/com/arom/with_travel/domain/member/Member.java
+++ src/main/java/com/arom/with_travel/domain/member/Member.java
@@ -1,4 +1,56 @@
 package com.arom.with_travel.domain.member;
 
-public class Member {
+import com.arom.with_travel.domain.shorts.Shorts;
+import com.arom.with_travel.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE member SET deleted = true where id = ?")
+@SQLRestriction("deleted is FALSE")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull private String oauthId;
+    @NotNull private String email;
+    @NotNull private LocalDate birth;
+    @NotNull @Enumerated(EnumType.STRING) private Gender gender;
+    @NotNull private String phone;
+    @NotNull @Enumerated(EnumType.STRING) private LoginType loginType;
+    @NotNull private String nickname;
+    @NotNull @Lob private String introduction;
+    @NotNull @Enumerated(EnumType.STRING) private TravelType travelType;
+    private boolean deleted = Boolean.FALSE;
+
+    public enum TravelType {
+        USER,
+        ADMIN
+    }
+
+    public enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    public enum LoginType {
+        NAVER,
+        KAKAO
+    }
+
+    @OneToMany(mappedBy = "member")
+    private List<Shorts> shorts = new ArrayList<>();
 }
