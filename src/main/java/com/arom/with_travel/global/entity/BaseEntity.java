d260d5689fb259545abd8f6ed439445bdb43428a
LeeDongHoon <likelion.leedh@gmail.com>
2025-01-20 15:58:41 +0900

    [feat] BaseEntity 설계

---
+++ src/main/java/com/arom/with_travel/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.arom.with_travel.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
